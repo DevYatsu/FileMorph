f259679760058ecba47e7896aa3c067adf6faee1
Yatsu <yatsu.dev@gmail.com>
2023-05-31 15:53:17 +0200

    change in the way the file is saved in the computer, now you choose the name of the file and you can put it wherever you want in your desktop folders

--- src-tauri/src/convertions.rs
+++ src-tauri/src/convertions.rs
@@ -1,20 +1,51 @@
-use converter_buddy::{io::ConvertibleFile, format::Format};
-use std::fs;
+use converter_buddy::{io::ConvertibleFile, format::Format, converter::{ConversionError, Converter}};
+use std::{fs::{self, File}, io::{Read, Write}};
 
 use crate::files_utils::get_format;
 
+trait ConvertionFeatures {
+    fn convert_with_target(&self, target_format: Format, output_path: &str) ->Result<File, ConversionError>;
+}
+
+impl ConvertionFeatures for ConvertibleFile {
+    fn convert_with_target(&self, target_format: Format, output_path: &str) -> Result<File, ConversionError> {
+        let source_format = self.format().ok_or(ConversionError::UnknownSourceFormat)?;
+
+        // let target_format_ext = target_format.info().preferred_extension;
+        // let full_target_ext: String = String::from("cb.") + target_format_ext;
+        // let full_path = &self.path.with_extension(full_target_ext);
+
+        let mut src_file = File::open(&self.path).map_err(ConversionError::IoError)?;
+        let mut target_file = File::create(output_path).map_err(ConversionError::IoError)?;
+
+        let mut input_buffer = Vec::<u8>::new();
+        let mut output_buffer = Vec::<u8>::new();
+
+        // read buffer from source file
+        src_file
+            .read_to_end(&mut input_buffer)
+            .map_err(ConversionError::IoError)?;
+
+        let converter = Converter::try_from(source_format)?;
+        converter.process(&input_buffer, &mut output_buffer, target_format.try_into()?)?;
+
+        target_file
+            .write_all(&output_buffer)
+            .map_err(ConversionError::IoError)?;
+        Ok(target_file)
+    }
+
+}
+
 #[tauri::command]
 pub fn convert_file(path: &str, output_format: &str, output_path: &str) -> bool {
-    let source_path: &str = path;    
-    let destination_path: &str = output_path;
-    println!("{}, {}", output_path, destination_path);
-    fs::copy(source_path, destination_path).unwrap();
+    println!("{}, {}", output_path, path);
 
-    let file: ConvertibleFile = ConvertibleFile::new(destination_path.to_lowercase().as_str());
+    let file: ConvertibleFile = ConvertibleFile::new(path.to_lowercase().as_str());
     file.format().expect("format input dude");
     let target_format: Format = get_format(output_format).unwrap();
 
-    match file.convert(target_format) {
+    match file.convert_with_target(target_format, output_path) {
         Ok(_) => true,
         Err(_) => false,
     }
