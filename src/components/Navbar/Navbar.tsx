648a1f421c3ea0afd19fad8d0052b867ec8ab08a
Yatsu <yatsu.dev@gmail.com>
2023-06-02 05:57:42 +0200

    adding of a github link in the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import "./Navbar.scss";
+import { FaGithub } from "react-icons/fa";
 
 function NavLogo() {
   return (
@@ -10,16 +11,27 @@ function NavLogo() {
   );
 }
 
+function GitLink() {
+  return (
+    <a href="https://github.com/DevYatsu/fileMorph" target="_blank">
+      <FaGithub className="w-8 h-full transition hover:scale-95" />
+    </a>
+  );
+}
+
 function Navbar() {
   return (
     <nav className="w-full h-24 bg-orange-200 border-gray-200 dark:bg-orange-700">
       <div className="flex flex-wrap items-center justify-center h-full mx-auto">
-        <a href="#" className="flex items-center h-full">
+        <div className="flex items-center h-full">
           <NavLogo />
-          <span className="self-center text-2xl font-semibold select-none whitespace-nowrap dark:text-white">
+          <span className="self-center pl-3 text-2xl font-semibold select-none whitespace-nowrap dark:text-white">
             FileMorph
           </span>
-        </a>
+          <div className="absolute right-5 md:right-12">
+            <GitLink />
+          </div>
+        </div>
       </div>
     </nav>
   );
