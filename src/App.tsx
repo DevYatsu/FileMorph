4b0f142b88ca3ad5f13afeb612fdb68dbd013ba1
IronYatsu <yatsu.dev@gmail.com>
2023-06-04 18:24:28 +0200

    removal of the reload context menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import MainSection from "./components/MainSection";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    if (window.location.hostname !== "tauri.localhost") {
+      return;
+    }
+
+    document.addEventListener("contextmenu", (e) => e.preventDefault());
+    document.addEventListener("selectstart", (e) => e.preventDefault());
+
+    return () => {
+      document.removeEventListener("contextmenu", (e) => e.preventDefault());
+      document.removeEventListener("selectstart", (e) => e.preventDefault());
+    };
+  }, []);
+
   return (
     <div className="flex flex-col w-full h-full text-black bg-gray-100 dark:text-white dark:bg-slate-900">
       <Navbar />
